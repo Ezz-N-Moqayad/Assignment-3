25c18e898c0653bc6b50f25e5cd62fc72e72437e
Ezz-N-Moqayad <ezz.n.moq@gmail.com>
2022-11-30 02:11:31 +0200

    V 1.11.30

--- routes/web.php
+++ routes/web.php
@@ -8,31 +8,31 @@ Auth::routes();
 
 // Teacher
 
-Route::get('teacher/create', 'Teacher\TeacherController@create');
-Route::post('teacher/store', 'Teacher\TeacherController@store');
-Route::get('', 'Teacher\TeacherController@index');
-Route::get('teacher/edit/{id}', 'Teacher\TeacherController@edit');
-Route::put('teacher/update/{id}', 'Teacher\TeacherController@update');
-Route::post('teacher/delete/{id}', 'Teacher\TeacherController@destroy');
-Route::get('teacher/info', 'Teacher\TeacherController@indexInfo');
+Route::get('teacher/create', 'Teacher\TeacherController@create')->middleware('auth');
+Route::post('teacher/store', 'Teacher\TeacherController@store')->middleware('auth');
+Route::get('', 'Teacher\TeacherController@index')->middleware('auth');
+Route::get('teacher/edit/{id}', 'Teacher\TeacherController@edit')->middleware('auth');
+Route::put('teacher/update/{id}', 'Teacher\TeacherController@update')->middleware('auth');
+Route::post('teacher/delete/{id}', 'Teacher\TeacherController@destroy')->middleware('auth');
+Route::get('teacher/info', 'Teacher\TeacherController@indexInfo')->middleware('auth');
 
 
 // Course
 
-Route::get('course/create', 'Course\CourseController@create');
-Route::post('course/store', 'Course\CourseController@store');
-Route::get('course', 'Course\CourseController@index');
-Route::get('course/edit/{id}', 'Course\CourseController@edit');
-Route::put('course/update/{id}', 'Course\CourseController@update');
-Route::post('course/delete/{id}', 'Course\CourseController@destroy');
-Route::get('course/info', 'Course\CourseController@indexInfo');
+Route::get('course/create', 'Course\CourseController@create')->middleware('auth');
+Route::post('course/store', 'Course\CourseController@store')->middleware('auth');
+Route::get('course', 'Course\CourseController@index')->middleware('auth');
+Route::get('course/edit/{id}', 'Course\CourseController@edit')->middleware('auth');
+Route::put('course/update/{id}', 'Course\CourseController@update')->middleware('auth');
+Route::post('course/delete/{id}', 'Course\CourseController@destroy')->middleware('auth');
+Route::get('course/info', 'Course\CourseController@indexInfo')->middleware('auth');
 
 
 // Student
 
-Route::get('student/create', 'Student\StudentController@create');
-Route::post('student/store', 'Student\StudentController@store');
-Route::get('student', 'Student\StudentController@index');
-Route::get('student/edit/{id}', 'Student\StudentController@edit');
-Route::put('student/update/{id}', 'Student\StudentController@update');
-Route::post('student/delete/{id}', 'Student\StudentController@destroy');
+Route::get('student/create', 'Student\StudentController@create')->middleware('auth');
+Route::post('student/store', 'Student\StudentController@store')->middleware('auth');
+Route::get('student', 'Student\StudentController@index')->middleware('auth');
+Route::get('student/edit/{id}', 'Student\StudentController@edit')->middleware('auth');
+Route::put('student/update/{id}', 'Student\StudentController@update')->middleware('auth');
+Route::post('student/delete/{id}', 'Student\StudentController@destroy')->middleware('auth');
